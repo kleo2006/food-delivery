9bc7bcfb07c36dea43c29ad75fd0d5fb6241c0c3
Kleo Fili <kleofili60@gmail.com>
2026-03-18 14:13:09 +0100

    initial commit

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import "./Header.css"
+  
+  
+
+const Header = () => {
+   const handleViewMenu =()=>{
+    document.getElementById('explore-menu').scrollIntoView({behavior:"smooth"})
+   }
+  return (
+    <div className="header">
+    <div className="header-contents">
+      <h2>Order your favorite food here</h2>
+      <p>Choose from a diverse menu featuring a delectable array of dishes crafted with the finest ingridients and culinary expertise. Our mission is to satisfy your cravings and
+         elevate your dining experience, one delicious meal at a time.</p>
+         <button onClick={handleViewMenu}>View Menu</button>
+    </div>
+      
+    </div>
+  )
+}
+
+export default Header
