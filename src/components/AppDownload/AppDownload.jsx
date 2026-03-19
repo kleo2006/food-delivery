ed9a8c2df31c0cc66d4ae9edbc1e14a72ecb4a02
Kleo Fili <kleofili60@gmail.com>
2026-03-19 11:07:16 +0100

    make app store links clickable

--- src/components/AppDownload/AppDownload.jsx
+++ src/components/AppDownload/AppDownload.jsx
@@ -1,17 +1,22 @@
 import React from 'react'
 import "./AppDownload.css"
 import { assets } from '../../assets/frontend_assets/assets';
-
+ 
 const AppDownload = () => {
   return (
     <div className="app-download" id="app-download">
-      <p>For better experience Download <br/>Tomato App</p>
-    <div className="app-download-platforms">
-        <img  src={assets.play_store} alt="" />
-        <img src={assets.app_store} alt="" />
-    </div>
+      <p>For better experience Download <br />Tomato App</p>
+      <div className="app-download-platforms">
+        <a href="https://play.google.com/store" target="_blank" rel="noreferrer">
+          <img src={assets.play_store} alt="Get it on Google Play" />
+        </a>
+        <a href="https://www.apple.com/app-store/" target="_blank" rel="noreferrer">
+          <img src={assets.app_store} alt="Download on the App Store" />
+        </a>
+      </div>
     </div>
   )
 }
-
+ 
 export default AppDownload
+ 
